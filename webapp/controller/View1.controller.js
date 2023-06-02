f3dc5ec2bac9ba1006669f421226341e4e210e63
Thomas Paflias <tpaflias@deloitte.gr>
2023-06-02 15:30:28 +0000

    added search functionality

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,64 +1,115 @@
+// @ts-nocheck
 sap.ui.define(
   [
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/format/DateFormat",
     "sap/ui/core/format/NumberFormat",
-    "sap/ui/core/routing/History",
   ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
+   * @param {typeof sap.ui.core.format.DateFormat} DateFormat
+   * @param {typeof sap.ui.core.format.NumberFormat} NumberFormat
    */
   function (Controller, DateFormat) {
     "use strict";
 
     return Controller.extend("project1.controller.View1", {
-      onInit: function () {},
+      onInit: function () {
+        // Initialize the default filter key
+        this._sFilterKey = "all";
+      },
+
+      onFilterChange: function (event) {
+        // Get the selected key from the event
+        const selectedKey = event.getParameter("selectedItem").getKey();
+        // Update the filter key
+
+        this._sFilterKey = selectedKey;
+      },
+
+      onSearch: function (event) {
+        // Get the search term
+        const searchTerm = event.getParameter("query");
+        // Get the table and its binding
+        const table = this.byId("tableId");
+        const binding = table.getBinding("items");
+
+        if (searchTerm && binding) {
+          // Create filters based on the search term and filter key
+          const filters = [];
+
+          if (this._sFilterKey !== "all") {
+            filters.push(
+              new sap.ui.model.Filter(
+                this._sFilterKey,
+                sap.ui.model.FilterOperator.Contains,
+                searchTerm
+              )
+            );
+          }
+
+          // Apply the filters to the table binding
+
+          binding.filter(filters);
+        } else {
+          // Clear the filters
+
+          binding.filter([]);
+        }
+      },
+
       formatDate: function (value) {
         if (value) {
-          var oDateFormat = DateFormat.getDateInstance({
+          // Format the date using the specified pattern
+          const oDateFormat = DateFormat.getDateInstance({
             pattern: "dd MMM yy",
           });
           return oDateFormat.format(new Date(value));
         }
         return value;
       },
+
       formatBaggageAllowance: function (value) {
         if (value) {
           if (value === "No baggage allowed.") {
-            return "Not Allowed";
+            return "Not Allowed.";
           } else {
-            var floatValue = parseFloat(value);
-            return floatValue.toFixed(2) + " Kg";
+            // Parse the value as a float and format it with 2 decimal places
+            const floatValue = parseFloat(value);
+            return `${floatValue.toFixed(2)} Kg`;
           }
         }
         return value;
       },
+
       formatFlightDuration: function (value) {
         if (value) {
-          var hours = Math.floor(value / 60);
-          var minutes = value % 60;
-          return hours + "hr " + minutes + "min";
+          // Calculate hours and minutes from the duration value
+          const hours = Math.floor(value / 60);
+          const minutes = value % 60;
+          return `${hours}hr ${minutes}min`;
         }
         return value;
       },
+
       formatTime: function (value) {
         if (value) {
-          var oDateFormat = DateFormat.getTimeInstance({ style: "short" });
-          var time = new Date(value.ms);
+          // Format the time using the "short" style
+          const oDateFormat = DateFormat.getTimeInstance({ style: "short" });
+          const time = new Date(value.ms);
           return oDateFormat.format(time);
         }
         return value;
       },
 
-      formatCityName: function (value) {
+      formatCityName: (value) => {
         if (value) {
-          var words = value.split(" ");
-          for (var i = 0; i < words.length; i++) {
-            words[i] =
-              words[i].charAt(0).toUpperCase() +
-              words[i].slice(1).toLowerCase();
-          }
-          return words.join(" ");
+          // Capitalize each word in the city name
+          const words = value.split(" ");
+          const capitalizedWords = words.map((word) => {
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+          });
+          return capitalizedWords.join(" ");
         }
         return value;
       },
